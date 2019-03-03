9c0b31a75a8abaa8602082c4446a2156e51fd946
Thomas Brooks <brooks.thomas67@gmail.com>
2019-03-02 21:21:37 -0800

    Added Main Menu and Character Generator scenes with basic scene manager

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public void LoadSceneByIndex(int index) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
